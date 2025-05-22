554f065fbae58461cb7ba868e78d07fa32491385
gatlingjakob <gatlingjakob@hotmail.com>
2025-05-23 00:46:25 +0200

    navbar

--- scripts/script-root.js
+++ scripts/script-root.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Load navbar for root page
-  fetch('navbar.html')  // navbar.html in root folder
+  fetch('navbar-index.html')  // navbar.html in root folder
     .then(res => res.text())
     .then(html => {
-      document.getElementById('navbar').innerHTML = html;
+      document.getElementById('navbar-index').innerHTML = html;
     })
     .catch(err => {
       console.error('Failed to load navbar:', err);
